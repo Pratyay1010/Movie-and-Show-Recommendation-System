3397300f253607471dc47e4a98ab2fa7952a7b9e
Pratyay1010 <59337443+Pratyay1010@users.noreply.github.com>
2023-01-07 15:57:59 +0530

    Similarity file added

---
+++ pages/external_files/recommender.py
@@ -0,0 +1,14 @@
+import pickle
+
+show_df = pickle.load(open("pages/external_files/Pickle_Files/shows_df.pkl", "rb"))
+similarities = pickle.load(open("pages/external_files/Pickle_Files/similarities.pkl", "rb"))
+
+
+def recommend_show(title):
+    index = show_df[show_df["title"] == title].index[0]
+
+    recommendation_list = []
+    for i in sorted(list(enumerate(similarities[index])), key=lambda x: x[1], reverse=True)[1:11]:
+        recommendation_list.append(show_df.iloc[i[0]]["title"])
+
+    return recommendation_list
